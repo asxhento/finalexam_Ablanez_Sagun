0130710c99faeb5368b3d627dac5b269652e817a
lee <ablanezdaryllee@gmail.com>
2026-05-28 16:16:35 +0800

    route_prog2done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\employeecontroller;
+use Illuminate\Support\Facades\Auth;    
+use App\Http\Controllers\employeecontroller;   
 
 
 
